cba189e6d74b6c0b89f07f96c2ff3524353d7674
JavaBySandeep <42215856+javabysandeep@users.noreply.github.com>
2024-04-13 12:05:23 +0530

    collection framework

---
+++ core-java/src/collectionFramework/Demo1LinkedList.java
@@ -0,0 +1,48 @@
+package collectionFramework;
+
+public class Demo1LinkedList {
+    public static void main(String[] args) {
+        //single linked list
+        class Node {
+            int data;
+            Node next;
+
+            Node(int data) {
+                this.data = data;
+            }
+        }
+        class LinkedList {
+            Node first = null;
+            Node last = null;
+            int size = 0;
+
+            void add(int element) {
+                Node newNode = new Node(element);
+                size++;
+                if (first == null) {
+                    first = last = newNode;
+                } else {
+                    last.next = newNode;
+                    last = newNode;
+                }
+            }
+
+            void print() {
+                Node temp = first;
+                while (temp != null) {
+                    System.out.println(temp.data + " -> ");
+                    temp = temp.next;
+                }
+            }
+        }
+
+        LinkedList linkedList = new LinkedList();
+        linkedList.add(10);
+        linkedList.add(20);
+        linkedList.add(30);
+        linkedList.add(40);
+        linkedList.add(50);
+        linkedList.print();
+        System.out.println("Size of the list is "+linkedList.size);
+    }
+}
---
+++ core-java/src/collectionFramework/Demo2.java
@@ -0,0 +1,9 @@
+package collectionFramework;
+
+import java.util.LinkedList;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        //LinkedList
+    }
+}
