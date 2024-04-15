ffc6188a09c367ada7d8f45914903329beaf3a26
JavaBySandeep <42215856+javabysandeep@users.noreply.github.com>
2024-04-15 10:49:51 +0530

    collection 2

---
+++ core-java/src/collectionFramework/Demo3ArrayList.java
@@ -0,0 +1,62 @@
+package collectionFramework;
+
+
+public class Demo3ArrayList {
+    public static void main(String[] args) {
+
+        //ds definition
+        class ArrayList {
+            Object[] values;
+            int size = 0;
+            int capacity = 10;
+
+            ArrayList() {
+                values = new Object[capacity];
+            }
+
+            public void add(Object object) {
+                if (capacity > size) {
+                    values[size++] = object;
+                } else {
+                    capacity *= 2;
+                    Object[] newArray = new Object[capacity];
+                    //copy old array into new array
+                    for (int index = 0; index < values.length; index++) {
+                        newArray[index] = values[index];
+                    }
+                    newArray[size++] = object;
+                    values = newArray;
+                }
+            }
+        }
+
+        //to store multiple elements with shortage of memory
+        ArrayList arrayList = new ArrayList();//ds creation
+        arrayList.add(100);
+        arrayList.add(200);
+        arrayList.add("abc");
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(100);
+        System.out.println("Size of the container "+arrayList.size);
+        System.out.println("capacity of the container "+arrayList.capacity);
+
+
+    }
+}
---
+++ core-java/src/collectionFramework/Demo4.java
@@ -0,0 +1,11 @@
+package collectionFramework;
+
+import java.util.ArrayList;
+
+public class Demo4 {
+    //Iterable
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        arrayList.iterator();
+    }
+}
